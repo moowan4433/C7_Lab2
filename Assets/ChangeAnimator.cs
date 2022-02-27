4c451a10ee69a46603df68b9e2f56120cb7ce462
moowan4433 <93364172+moowan4433@users.noreply.github.com>
2022-02-28 02:54:04 +0700

    C7_Lab2_343

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    public void getAnimatorRoll()
+    {
+        this.GetComponent<Animator>().Play("Stand To Roll");
+    }
+
+    public void getAnimatorStand()
+    {
+        this.GetComponent<Animator>().Play("Standing W_Briefcase Idle");
+    }
+}
